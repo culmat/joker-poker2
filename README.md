48ca596f6bb62faa1baf8789cd43e3290009c043
Matthias Cullmann <matthias.cullmann@gmail.com>
2021-12-20 16:24:55 +0100

    initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Vue Todo MVC example
+
+This example demonstrates how you can use SyncedStore to build a collaborative version of the [Todo MVC](http://todomvc.com) app.
+
+The code is based on the [official Vue 3 Todo MVC example](https://v3.vuejs.org/examples/todomvc.html). Minimum changes were needed to make the App work with SyncedStore.
+
+# Live demo
+
+- [Open live demo](https://uie1c.csb.app/) (Of course, open multiple times to test multiplayer)
+- [Open source on Codesandbox](https://codesandbox.io/s/todo-vue-uie1c)
+
+# Running
+
+    npm install
+    npm run serve
+
+🔥 Make sure to open multiple browsers to see the app syncing automatically.
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+// Full spec-compliant TodoMVC with localStorage persistence
+// and hash-based routing in ~120 effective lines of JavaScript.
+
+import * as Vue from "vue";
+import App from "./App.vue";
+import { enableVueBindings } from "@syncedstore/core";
+
+// make SyncedStore use Vuejs internally
+enableVueBindings(Vue);
+
+const app = Vue.createApp(App);
+const vm = app.mount("#app") as any;
+
+function onHashChange() {
+  const visibility = window.location.hash.replace(/#\/?/, "");
+  if (["all", "active", "completed"].includes(visibility)) {
+    vm.visibility = visibility;
+  } else {
+    window.location.hash = "";
+    vm.visibility = "all";
+  }
+}
+
+window.addEventListener("hashchange", onHashChange);
+onHashChange();
+
+console.log("theApp", app);
